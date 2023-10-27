c39fb737f6c09c0b0651cb1e14d177d7dec0c71f
przemek <przemyslaw.leczycki@valueadd.pl>
2023-10-27 19:05:49 +0200

    feat: code optimization

--- apps/file-organizer/src/cli/file-organizer.ts
+++ apps/file-organizer/src/cli/file-organizer.ts
@@ -6,95 +6,84 @@ import {FileExtensionFilter} from "../file-filter/file-extension-filter";
 import {FileOrganizer} from "../file-organizer";
 import {
   CopyFileRelocator,
-  CreationDateDestinationBuilder,
+  CreationDateDestinationBuilder, FileRelocator,
   MoveFileRelocator
 } from "../file-path-converter/file-destination-builder";
+import {ConsoleLogger} from "../logger/logger-console";
 
 yargs(hideBin(process.argv))
   .usage('Usage: $0 <command> [options]')
   .example('$0 <command> -s ./source -d ./destination', 'Organize files')
   .command('move', 'Move files from source to destination', (yargs) => {
-    return yargs
-      .option('source', {
-        alias: 's',
-        type: 'string',
-        description: 'Source directory'
-      })
-      .option('destination', {
-        alias: 'd',
-        type: 'string',
-        description: 'Destination directory'
-      })
-      .option('format', {
-        alias: 'f',
-        type: 'string',
-        default: 'yyyy/mm',
-        description: 'Directory format e.g. yyyy/mm/dd'
-      })
-      .demandOption(['source', 'destination']);
+    return commandArguments(yargs);
   }, (argv) => {
     move(argv.source, argv.destination, argv.format).then();
   })
   .command('copy', 'Copy files from source to destination', (yargs) => {
-    return yargs
-      .option('source', {
-        alias: 's',
-        type: 'string',
-        description: 'Source directory'
-      })
-      .option('destination', {
-        alias: 'd',
-        type: 'string',
-        description: 'Destination directory'
-      })
-      .option('format', {
-        alias: 'f',
-        type: 'string',
-        default: 'yyyy/mm',
-        description: 'Directory format e.g. yyyy/mm/dd'
-      })
-      .demandOption(['source', 'destination']);
+    return commandArguments(yargs);
   }, (argv) => {
     copy(argv.source, argv.destination, argv.format).then();
   })
   .demandCommand()
   .argv;
 
-async function move(source: string, destination: string, format: string) {
-  const fileSystem = new NodeFileSystem();
-  const fileOrganizer = new FileOrganizer(
-    fileSystem,
-    new CreationDateDestinationBuilder(fileSystem, format),
-    new MoveFileRelocator(fileSystem),
-    [
-      new FileExtensionFilter(['jpg', 'jpeg', 'png']),
-      new FileExtensionFilter(['mp4', 'mov', 'avi'])
-    ]
-  );
-  await fileOrganizer.organizeFiles(
-    ExistingDirectory.existingValid(source, fileSystem),
-    ExistingDirectory.existingValid(destination, fileSystem)
-  );
-
-  process.exit(0);
+function commandArguments(yargs: yargs.Argv<object>) {
+  return yargs
+    .option('source', {
+      alias: 's',
+      type: 'string',
+      description: 'Source directory'
+    })
+    .option('destination', {
+      alias: 'd',
+      type: 'string',
+      description: 'Destination directory'
+    })
+    .option('format', {
+      alias: 'f',
+      type: 'string',
+      default: 'yyyy/mm',
+      description: 'Output directory structure e.g. yyyy/mm/dd'
+    })
+    .demandOption(['source', 'destination']);
 }
 
-async function copy(source: string, destination: string, format: string) {
+async function move(source: string, destination: string, format: string): Promise<never> {
+  return relocate(source, destination, format, 'move');
+}
+
+async function copy(source: string, destination: string, format: string): Promise<never> {
+  return relocate(source, destination, format, 'copy');
+}
+
+async function relocate(source: string, destination: string, format: string, method: 'copy' | 'move'): Promise<never> {
+  const logger = new ConsoleLogger();
   const fileSystem = new NodeFileSystem();
+  const relocator: FileRelocator =
+    method === 'copy'
+      ? new CopyFileRelocator(fileSystem)
+      : new MoveFileRelocator(fileSystem);
   const fileOrganizer = new FileOrganizer(
     fileSystem,
     new CreationDateDestinationBuilder(fileSystem, format),
-    new CopyFileRelocator(fileSystem),
+    relocator,
     [
       new FileExtensionFilter(['jpg', 'jpeg', 'png']),
       new FileExtensionFilter(['mp4', 'mov', 'avi'])
     ]
   );
 
-  await fileOrganizer.organizeFiles(
-    ExistingDirectory.existingValid(source, fileSystem),
-    ExistingDirectory.existingValid(destination, fileSystem)
-  );
+  try {
+    await fileOrganizer.organizeFiles(
+      ExistingDirectory.existingValid(source, fileSystem),
+      ExistingDirectory.existingValid(destination, fileSystem)
+    );
+  } catch (e) {
+    if (e instanceof Error) {
+      logger.error(e.message);
+    }
+    process.exit(1);
+  }
 
   process.exit(0);
 }
